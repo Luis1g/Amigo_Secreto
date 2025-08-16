f0687e46f2126e719e7cd22c16384efdfd2563c5
Luis gerardo <luishernandezroman78@gmail.com>
2025-08-16 00:52:04 -0600

    Actualizar archivo app

--- app.js
+++ app.js
@@ -13,13 +13,20 @@ function agregarAmigo(){
         alert("Por favor, ingrese un nombre");
         return
     }
+
+    // Contar cuántas veces ya aparece el nombre
+    let contador = amigos.filter(n => n.startsWith(nombreAmigo)).length;
+
+    if (contador > 0) {
+        // Si ya existe, le agregamos un número (Luis segundo, Luis tercero, etc.)
+        nombreAmigo = nombreAmigo + " " + (contador + 1);
+    }
+
     // Almacenamos los nombre en una variable llamada "amigos".
     amigos.push(nombreAmigo);
     // Limpiamos el campo, para agregar un nuevo nombre.
     document.getElementById("amigo").value = "";
-    
     actualizarLista();
-    
 }
 
 
@@ -31,4 +38,15 @@ function actualizarLista(){
         li.textContent = amigo;
         listaActualizada.appendChild(li);
     })
+}
+
+
+function sortearAmigo(){
+    if (amigos.length == 0){
+        alert("NO hay amigos disponibles");
+        return;
+    }
+    let numeroAmigo = Math.floor(Math.random() * amigos.length);
+    let amigoSecreto = amigos[numeroAmigo];
+    document.getElementById("resultado").textContent = "El amigo secreto es:" + amigoSecreto 
 }
